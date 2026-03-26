95e2a7b09fe1b8a458600e6c96a2815b75c6c0bc
Daniel Attali <danielattali16@gmail.com>
2026-03-26 14:15:12 +0200

    updated the pytorch code

--- 01_mlp/01_mlp_torch.py
+++ 01_mlp/01_mlp_torch.py
@@ -1,20 +1,19 @@
 """
 This python module is a for creating a PyTorch Neural Network for the MNIST dataset.
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
+import torch.optim as optim
+import tqdm
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-import matplotlib.pyplot as plt
-
-import numpy as np
-
-import tqdm
-
 device = torch.device("mps" if torch.mps.is_available() else "cpu")
+
 
 class MLP(nn.Module):
     def __init__(self):
@@ -37,50 +36,61 @@ class MLP(nn.Module):
     def set_optimizer(self, optimizer: optim.Optimizer) -> None:
         self.optimizer = optimizer
 
+    def train_epoch(self, train_loader: DataLoader, val_loader: DataLoader) -> tuple:
+        self.train()
+
+        train_loss_sum = 0.0
+        train_correct = 0
+        train_count = 0
+
+        for _, (images, labels) in tqdm.tqdm(
+                enumerate(train_loader),
+                total=len(train_loader),
+                desc="Train",
+                unit="batch",
+                leave=False,
+        ):
+            images, labels = images.to(device), labels.to(device)
+            self.optimizer.zero_grad()
+            outputs = self.forward(images)
+            loss = self.loss_function(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+
+            bs = images.size(0)
+            train_loss_sum += loss.item() * bs
+            train_correct += (outputs.argmax(dim=1) == labels).sum().item()
+            train_count += bs
+
+        train_loss = train_loss_sum / train_count
+        train_acc = train_correct / train_count
+
+        self.eval()
+        val_loss_sum = 0.0
+        val_correct = 0
+        val_count = 0
+
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = self.forward(images)
+                loss = self.loss_function(outputs, labels)
+
+                bs = images.size(0)
+                val_loss_sum += loss.item() * bs
+                val_correct += (outputs.argmax(dim=1) == labels).sum().item()
+                val_count += bs
+
+        val_loss = val_loss_sum / val_count
+        val_acc = val_correct / val_count
+        return train_loss, train_acc, val_loss, val_acc
+
     def train_model(self, train_loader: DataLoader, val_loader: DataLoader, epochs: int = 10) -> list:
         history = []
 
         pbar = tqdm.trange(epochs, desc="Training", unit="epoch")
         for epoch in pbar:
-            self.train()
-            train_loss_sum = 0.0
-            train_correct = 0
-            train_count = 0
-
-            for images, labels in train_loader:
-                images, labels = images.to(device), labels.to(device)
-
-                self.optimizer.zero_grad()
-                outputs = self.forward(images)
-
-                loss = self.loss_function(outputs, labels)
-                loss.backward()
-                self.optimizer.step()
-
-                bs = images.size(0)
-                train_loss_sum += loss.item() * bs
-                train_correct += (outputs.argmax(dim=1) == labels).sum().item()
-                train_count += bs
-
-            self.eval()
-            val_loss_sum = 0.0
-            val_correct = 0
-            val_count = 0
-            with torch.no_grad():
-                for images, labels in val_loader:
-                    images, labels = images.to(device), labels.to(device)
-                    val_outputs = self.forward(images)
-                    val_loss = self.loss_function(val_outputs, labels)
-
-                    bs = images.size(0)
-                    val_loss_sum += val_loss.item() * bs
-                    val_correct += (val_outputs.argmax(dim=1) == labels).sum().item()
-                    val_count += bs
-
-            train_loss = train_loss_sum / train_count
-            val_loss = val_loss_sum / val_count
-            train_acc = train_correct / train_count
-            val_acc = val_correct / val_count
+            train_loss, train_acc, val_loss, val_acc = self.train_epoch(train_loader, val_loader)
 
             history.append([epoch + 1, train_loss, val_loss, train_acc, val_acc])
             pbar.set_postfix(
@@ -110,10 +120,18 @@ class MLP(nn.Module):
 
         return total_loss / total_samples, total_correct / total_samples
 
+    def save_model(self, path: str = "models/mlp.pth") -> None:
+        print(f"Saving model to {path}")
+        torch.save(self.state_dict(), path)
+
+    def load_model(self, path: str = "models/mlp.pth") -> None:
+        print(f"Loading model from {path}")
+        self.load_state_dict(torch.load(path))
+
 
 def plot_mnist_images(images, labels, n_x: int = 3, n_y: int = 3) -> None:
     # plot a grid of n_x x n_y images using matplotlib
-    fig, axes = plt.subplots(n_x, n_y, figsize=(n_y*2, n_x*2))
+    fig, axes = plt.subplots(n_x, n_y, figsize=(n_y * 2, n_x * 2))
     for i in range(n_x):
         for j in range(n_y):
             random_idx = np.random.randint(0, len(images))
@@ -123,6 +141,7 @@ def plot_mnist_images(images, labels, n_x: int = 3, n_y: int = 3) -> None:
             axes[i, j].axis("off")
 
     plt.show()
+
 
 def plot_training_history(history: list) -> None:
     history = np.array(history)
@@ -187,6 +206,10 @@ def main():
     plot_training_history(history)
 
     print("Accuracy: {:.2f}%".format(acc * 100))
+
+    # save the model
+    mlp.save_model("models/mlp.pth")
+
 
 if __name__ == "__main__":
     main()
